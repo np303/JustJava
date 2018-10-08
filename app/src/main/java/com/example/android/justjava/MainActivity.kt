58f747f68eccbc43c6cfccadd9c42e43bc96a5ec
np303 <2ep303@gmail.com>
2018-10-08 13:23:14 -0500

    Button Works!

--- app/src/main/java/com/example/android/justjava/MainActivity.kt
+++ app/src/main/java/com/example/android/justjava/MainActivity.kt
@@ -1,12 +1,41 @@
+/**
+ * IMPORTANT: Make sure you are using the correct package name.
+ * This example uses the package name:
+ * package com.example.android.justjava
+ * If you get an error when copying this code into Android studio, update it to match teh package name found
+ * in the project's AndroidManifest.xml file.
+ */
+
 package com.example.android.justjava
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
 
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
+
+/**
+ * This app displays an order form to order coffee.
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    fun submitOrder(view: View) {
+        display(666)
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private fun display(number: Int) {
+        val quantityTextView = findViewById<View>(R.id.quantity_text_view) as TextView
+        quantityTextView.text = "" + number
     }
 }
